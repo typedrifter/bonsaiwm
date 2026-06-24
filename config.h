f4309d4abd7b2710f9bbbfc8cbfe440901956ddb
Francois Lachese <francois.lachese@gmail.com>
2026-06-24 09:47:29 +0200

    config: add layouts symbols

--- config.h
+++ config.h
@@ -44,9 +44,9 @@ static const Rule rules[] = {
 /* layout(s) */
 static const Layout layouts[] = {
     /* symbol     arrange function */
-    {"[]=", tile},
-    {"><>", NULL}, /* no layout function means floating behavior */
-    {"[M]", monocle},
+    {"T", tile},
+    {"F", NULL}, /* no layout function means floating behavior */
+    {"M", monocle},
 };
 
 /* monitors */
