ccfe6df6e6d44eec0232ce19041ad70c6ddd5a15
François Lachèse <francois.lachese@gmail.com>
2026-07-07 23:56:08 +0200

    config: format config

--- config.lua
+++ config.lua
@@ -1,13 +1,19 @@
-bonsaiwm.enablegaps = 1
-bonsaiwm.gappoh = 40
-bonsaiwm.gappov = 40
-bonsaiwm.gappih = 40
-bonsaiwm.gappiv = 40
-bonsaiwm.smartgaps = 0
+bonsaiwm = {
+  enablegaps = 1,
+  smartgaps = 0,
+  gappoh = 40,
+  gappov = 40,
+  gappih = 40,
+  gappiv = 40,
+  sloppyfocus = 1,
+  borderpx = 1,
+  repeat_rate = 25,
+  repeat_delay = 600,
 
--- colors: "#RRGGBB" or "#RRGGBBAA"
-bonsaiwm.rootcolor = "#1a1b26ff"
-bonsaiwm.bordercolor = "#414868ff"
-bonsaiwm.focuscolor = "#7aa2f7ff"
-bonsaiwm.urgentcolor = "#f7768eff"
-bonsaiwm.fullscreen_bg = "#1a1b26ff"
+  -- colors: "#RRGGBB" or "#RRGGBBAA"
+  rootcolor = "#1a1b26ff",
+  bordercolor = "#414868ff",
+  focuscolor = "#7aa2f7ff",
+  urgentcolor = "#f7768eff",
+  fullscreen_bg = "#1a1b26ff",
+}
