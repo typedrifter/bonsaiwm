259b88eaaa0eb726f34689d9b349b83626263540
Francois Lachese <francois.lachese@gmail.com>
2026-07-22 23:56:57 +0200

    refactor: remove Astro for the moment

--- site/src/content.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineCollection } from 'astro:content';
-import { docsSchema } from '@astrojs/starlight/schema';
-import { docsLoaderWithDefaultTitle } from './loaders/docs-with-default-title.ts';
-
-export const collections = {
-	docs: defineCollection({ loader: docsLoaderWithDefaultTitle(), schema: docsSchema() }),
-};
