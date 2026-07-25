8f0798e891e1a9373b0cd23c3f6f25f11c981457
Francois Lachese <francois.lachese@gmail.com>
2026-07-25 22:22:03 +0200

    config(lua): make bordercolor transparent for rounded corners

--- config.lua
+++ config.lua
@@ -23,7 +23,7 @@ bonsaiwm = {
 
 	-- colors: "#RRGGBB" or "#RRGGBBAA"
 	rootcolor = "#1a1b26ff",
-	bordercolor = "#414868ff",
+	bordercolor = "#41486800",
 	focuscolor = "#7aa2f7ff",
 	urgentcolor = "#f7768eff",
 	fullscreen_bg = "#1a1b26ff",
