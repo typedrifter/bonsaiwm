8b28eca2ca82b1bd56a30f8794cc7d515f2b3ec9
François Lachèse <francois.lachese@gmail.com>
2026-08-01 18:51:13 +0200

    fix: xdg clip offset regression when corner_radius is disabled

--- client.h
+++ client.h
@@ -142,10 +142,8 @@ static inline void client_get_clip(Client *c, struct wlr_box *clip) {
   }
 #endif
 
-  if (corner_radius > 0) {
-    clip->x = c->surface.xdg->geometry.x;
-    clip->y = c->surface.xdg->geometry.y;
-  }
+  clip->x = c->surface.xdg->geometry.x;
+  clip->y = c->surface.xdg->geometry.y;
 }
 
 static inline void client_get_geometry(Client *c, struct wlr_box *geom) {
